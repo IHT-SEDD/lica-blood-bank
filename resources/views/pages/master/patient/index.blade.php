4558fd8f29ce04edc2708ac20974caa7d759c3e7
adithya-666 <adithyaasus07@gmail.com>
2026-05-01 22:34:09 +0700

    Fitur Master Patient

---
+++ resources/views/pages/master/patient/index.blade.php
@@ -0,0 +1,112 @@
+@extends('layouts.master-page-layout')
+
+@section('datatable-header')
+<div class="d-flex align-items-center justify-content-center gap-2">
+  {{-- Select Role --}}
+  <div>
+    <select class="form-control" id="filter-role" name="filter-role" placeholder="Filter by role..."></select>
+  </div>
+
+  {{-- Date filter :begin --}}
+  <div>
+    <div class="input-group">
+      <span class="input-group-text" id="master-patient-table-date-filter">
+        <i data-lucide="calendar" class="align-middle flex-shrink-0"></i>
+      </span>
+      <input class="form-control master-patient-table-date-filter" aria-describedby="master-patient-table-date-filter"
+        data-date-format="d-m-Y" data-provider="flatpickr" data-range-date="true" type="text"
+        placeholder="Choose date range" />
+    </div>
+  </div>
+  {{-- Date filter :begin --}}
+</div>
+@endsection
+
+@section('datatable-content')
+<table class="table table-striped dt-responsive align-middle mb-0 master-patient-table" id="master-patient-table">
+  <thead class="thead-sm text-uppercase fs-xxs">
+    <tr>
+      <th>No</th>
+      <th>Name</th>
+      <th>Medical Record</th>
+      <th>Gender</th>
+      <th>Birthdate</th>
+      <th>Phone</th>
+      <th>Email</th>
+      <th>Address</th>
+      <th>Deleted At</th>
+      <th>Action</th>
+    </tr>
+  </thead>
+</table>
+@endsection
+
+@section('form-content')
+<form class="row g-2" id="add_new_patient" autocomplete="off">
+  {{-- Name --}}
+  <div class="col-lg-6">
+    <label class="form-label" for="name">Name
+      <span class="text-danger">*</span>
+    </label>
+    <input autocomplete="off" class="form-control" id="name" name="name" type="text" placeholder="your full name" />
+  </div>
+
+  {{-- Username --}}
+  <div class="col-lg-6">
+    <label class="form-label" for="username">Username
+      <span class="text-danger">*</span>
+    </label>
+    <input autocomplete="off" class="form-control" id="username" name="username" type="text"
+      placeholder="your username" />
+  </div>
+
+  {{-- Email --}}
+  <div class="col-lg-12">
+    <label class="col-form-label" for="email">Email</label>
+    <input autocomplete="off" class="form-control" id="email" name="email" type="email"
+      placeholder="youremail@mail.com" />
+  </div>
+
+  {{-- Password --}}
+  <div class="col-lg-8">
+    <label class="form-label" for="password">Password
+      <span class="text-danger">*</span>
+    </label>
+    <input autocomplete="new-password" class="form-control" id="password" name="password" placeholder="••••••••"
+      type="password" />
+  </div>
+
+  {{-- Role --}}
+  <div class="col-lg-4">
+    <label class="form-label" for="select-role">Role
+      <span class="text-danger">*</span>
+    </label>
+    <select class="form-control" id="select-role" name="role" placeholder="Choose role..."></select>
+  </div>
+
+  {{-- Is Active? --}}
+  <div class="col-lg-3">
+    <div>
+      <div class="form-check form-check-info my-1">
+        <input checked="" class="form-check-input" id="is_active" name="is_active" type="checkbox" />
+        <label class="form-check-label" for="is_active">Active?</label>
+      </div>
+    </div>
+  </div>
+
+  {{-- Submit Button --}}
+  <div class="col-lg-12">
+    <button class="btn btn-primary" type="submit">Add New User</button>
+  </div>
+</form>
+@endsection
+
+@section('modal-content')
+@include('pages.master.user.partials.edit-data-modal')
+@include('pages.master.user.partials.delete-data-modal')
+@include('pages.master.user.partials.restore-data-modal')
+@endsection
+
+@section('custom-scripts')
+
+@endsection
---
+++ resources/views/pages/master/patient/partials/restore-data-modal.blade.php
@@ -0,0 +1,20 @@
+<x-static-modal-layout id="restore_data_master_user_modal" size="" title="Restore Data User">
+ <h4 class="fw-semibold">Are your sure want to restore this data?</h4>
+ <p>Data you want to restore: <span id="restored_data" class="text-capitalize text-muted"></span></p>
+ <p class="m-0">Click <span class="badge badge-label badge-soft-success">Confirm Restore</span> if you want to restore
+  it and click <span class="badge badge-label badge-soft-danger">Cancel Restore</span> if you want to cancel it
+ </p>
+
+ <hr />
+
+ <div class="d-flex align-items-center justify-content-end mt-2 gap-2">
+  {{-- Cancel Button --}}
+  <div>
+   <button class="btn btn-danger" data-bs-dismiss="modal" type="button">Cancel Restore</button>
+  </div>
+  {{-- Confirm Button --}}
+  <div>
+   <button class="btn btn-success" id="confirm_restore" type="submit">Confirm Restore</button>
+  </div>
+ </div>
+</x-static-modal-layout>
