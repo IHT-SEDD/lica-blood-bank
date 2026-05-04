d2d8a8b4aa34ab2f746ccfc58afc81203e62a8d8
zulfarobbani <robbanizulfa86@gmail.com>
2026-05-04 08:47:13 +0700

    feat:master Insurance and master doctor

---
+++ app/Models/Insurance.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
+
+class Insurance extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'public_id',
+        'is_active',
+        'name'
+
+    ];
+
+    protected $hidden = [
+        'id',
+    ];
+
+    protected static function booted()
+    {
+        static::creating(function ($insurance) {
+            if (empty($insurance->public_id)) {
+                $insurance->public_id = (string) Str::uuid();
+            }
+        });
+    }
+}
---
+++ resources/js/pages/master/doctor/index.js
@@ -0,0 +1,130 @@
+import TomSelect from "tom-select";
+import { GlobalSubmitForm, GlobalFormValidation } from "../../../app";
+
+// ---------- Global variable untuk memudahkan penyesuaian :begin ----------
+const FormAddSelector = "add_new_doctor"; // id selector untuk form add new
+const FormAddURL = "/master/doctor"; // url submit form add
+const FormEditSelector = "edit_data_doctor"; // id selector untuk form edit
+const ReloadDatatableSelector = "master-doctor-reload"; // reload datatable index
+const ModalEditSelector = "edit_data_master_doctor_modal"; // id selector untuk modal edit
+// ---------- Global variable untuk memudahkan penyesuaian :end ----------
+
+// ---------- Select role dari tom-select untuk form add new data :begin ----------
+// function SelectRole() {
+//     new TomSelect("#select-role", {
+//         valueField: "id",
+//         labelField: "text",
+//         searchField: "text",
+//         sortField: {
+//             field: "id",
+//             direction: "asc",
+//         },
+//         create: false,
+//         preload: true,
+//         load: function(query, callback) {
+//             fetch(`/utility/select/role?q=${encodeURIComponent(query)}`)
+//                 .then((response) => response.json())
+//                 .then((json) => {
+//                     callback(json.results);
+//                 })
+//                 .catch(() => {
+//                     callback();
+//                 });
+//         },
+//     });
+// }
+// ---------- Select role dari tom-select untuk form add new data :begin ----------
+
+// ---------- Handle form penambahan user baru :begin ----------
+function AddNewDoctor() {
+    // ---------- Validasi inputan form :begin ----------
+    const AddNewDoctorValidation = GlobalFormValidation.init(
+        "#" + FormAddSelector, {
+            name: {
+                validators: {
+                    notEmpty: {
+                        message: "Name is required",
+                    },
+                },
+            }
+        },
+    );
+    // ---------- Validasi inputan form :end ----------
+
+    // ---------- Submit form ke url :begin ----------
+    new GlobalSubmitForm({
+        formId: FormAddSelector,
+        url: FormAddURL,
+        validator: AddNewDoctorValidation,
+        onSuccess: (data) => {
+            notyf.success({
+                message: "New doctor added succesfully!",
+            });
+            console.log(data);
+            window.dispatchEvent(new Event(ReloadDatatableSelector));
+        },
+        onError: (err) => {
+            notyf.error({
+                message: "New doctor failed to insert!",
+            });
+
+            console.error(err);
+        },
+
+        resetOnSuccess: true,
+    });
+    // ---------- Submit form ke url :end ----------
+}
+// ---------- Handle form penambahan user baru :begin ----------
+
+// ---------- Handle form pembaharuan data user :begin ----------
+function EditDataDoctor() {
+    // ---------- Validasi inputan form :begin ----------
+    const EditDataDoctorValidation = GlobalFormValidation.init(
+        "#" + FormAddSelector, {
+            name: {
+                validators: {
+                    notEmpty: {
+                        message: "Name is required",
+                    },
+                },
+            }
+        },
+    );
+    // ---------- Validasi inputan form :end ----------
+
+    // ---------- Submit form ke url :begin ----------
+    new GlobalSubmitForm({
+        formId: FormEditSelector,
+        url: () => {
+            const form = document.getElementById(FormEditSelector);
+            return FormAddURL + `/${form.dataset.id}`;
+        },
+        method: "PATCH",
+        validator: EditDataDoctorValidation,
+        onSuccess: (data) => {
+            notyf.success({
+                message: "Data doctor updated succesfully!",
+            });
+            window.dispatchEvent(new Event(ReloadDatatableSelector));
+            const modalEl = document.getElementById(ModalEditSelector);
+            const modal = bootstrap.Modal.getInstance(modalEl);
+            modal.hide();
+        },
+        onError: (err) => {
+            notyf.error({
+                message: "Data doctor failed to update!",
+            });
+        },
+
+        resetOnSuccess: true,
+    });
+    // ---------- Submit form ke url :end ----------
+}
+// ---------- Handle form pembaharuan data user :begin ----------
+
+document.addEventListener("DOMContentLoaded", function() {
+    // SelectRole();
+    AddNewDoctor();
+    EditDataDoctor();
+});
